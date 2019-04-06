9ac4d900ca1cb3b3cdbbffa281ac5b7ac3ffcc96
Ryan Pethel <arpethel@gmail.com>
2019-04-06 12:43:38 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,36 +7,20 @@ class Anagram
 
   def initialize (word)
     @word = word
-    # binding.pry
   end
 
   def match(array)
-    # binding.pry
     array.each do |word|
-      binding.pry
       if word.size == @word.size
-        # binding.pry
         word = word.split(//)
-        binding.pry
         # if word.sort == @word.sort
         #   binding.pry
         #   true
         # else
         #   false
         # end
-
-        # word.each do |letter|
-        #   # binding.pry
-        #   if @word.include?(letter)
-        #     binding.pry
-        #     true
-        #   else
-        #     false
-        #   end
-        # end
       end
     end
-
   end
 
 end
