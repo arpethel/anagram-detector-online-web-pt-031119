6966bb3803a560f2649c9662b414f7a41844f1a7
Ryan Pethel <arpethel@gmail.com>
2019-04-06 14:16:20 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,23 +5,18 @@ class Anagram
 
   attr_accessor :word
 
-  # WORDS = []
-
   def initialize (word)
     @word = word
     @@words = []
-    # binding.pry
   end
 
   def match(array)
     array.select do |word|
-      # binding.pry
       if word.size == @word.size
         word_split = word.split(//)
         @word_split = @word.split(//)
         word_split.sort == @word_split.sort
         @@words << word
-        # binding.pry
       else
         @@words
       end
