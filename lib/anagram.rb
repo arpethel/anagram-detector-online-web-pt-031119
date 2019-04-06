01467383c9cc9ca5890beadd8d8d7b47fc29a365
Ryan Pethel <arpethel@gmail.com>
2019-04-06 12:46:11 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,15 +11,16 @@ class Anagram
 
   def match(array)
     array.each do |word|
-      binding.pry
+      # binding.pry
       if word.size == @word.size
         word = word.split(//)
-        # if word.sort == @word.sort
-        #   binding.pry
-        #   true
-        # else
-        #   false
-        # end
+        @word = @word.split(//)
+        if word.sort == @word.sort
+          binding.pry
+          true
+        else
+          false
+        end
       end
     end
   end
